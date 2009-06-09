a80763f291ba4730f4a536dd5bd51580d4fd92dd
Noah Fischer <nfische@sandia.gov>
2009-06-09 14:09:07 -0700

    Added supermon director and files to drivers/

---
+++ drivers/supermon/supermon_proc.h
@@ -0,0 +1,6 @@
+typedef int (*superproc_callback) (struct seq_file *seq);
+extern int superproc_register(superproc_callback info,
+			      superproc_callback value, int timeout);
+extern int superproc_unregister(int handle);
+
+#define PRINTIT(arg...) { seq_printf(seq, arg);}
